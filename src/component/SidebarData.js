ce114f6e37801e9ecd4095d3f392993c44fee43e
kanary <kanary159357@gmail.com>
2020-08-22 17:37:27 +0900

    테스트?

---
+++ src/component/SidebarData.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from  'react-icons/io';
+
+export const SidebarData= [
+    {
+        title: 'Home',
+        path:'/',
+        icon: <AiIcons.AiFillHome/>,
+        cName:'nav-text'
+    },
+
+    {
+        title: 'Reports',
+        path:'/reports',
+        icon: <IoIcons.IoIosPaper/>,
+        cName:'nav-text'
+    },
+    {
+        title: 'Products',
+        path:'/products',
+        icon: <FaIcons.FaCartPlus/>,
+        cName:'nav-text'
+    },
+]
